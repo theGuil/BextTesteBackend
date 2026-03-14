236368833ce467dd8d03657a9fe5d5bb51b8f6c8
theGuil <seflanguilhermesouza@gmail.com>
2026-03-14 13:12:37 -0300

    ✨ feat: add doc erd da collection lista e tarefa

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import helpers from './helpers/helpers';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import package_json from "../package.json"
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -30,6 +31,10 @@ app.get('/api/test', (req: Request, res: Response) => {
     helpers.set_response.res.SUCCESS({ res, message: "Sucesso ao rodar api!", results: [] });
 });
 
+app.get('/', (req: Request, res: Response) => {
+    helpers.set_response.res.SUCCESS({ res, message: `Version API: ${package_json.version}`, results: [] });
+});
+
 app.use((err: any, req: Request, res: Response, next: any) => {
     return helpers.set_response.res.SERVER_ERROR({ error: err, res });
 });
@@ -38,7 +43,7 @@ app.use((err: any, req: Request, res: Response, next: any) => {
 helpers.db.start()
 
 app.listen(PORT, () => {
-    console.log(`Servidor BextTeste rodando na porta: ${PORT}`);
+    console.log(`Servidor online em: http://localhost:${PORT}`);
     console.log(`Documentação API em: http://localhost:${PORT}/docs/api`);
     console.log(`Documentação ERD em: http://localhost:${PORT}/docs/erd?showMode=ALL_FIELDS`);
 });
